85718854d3449254dc8bc24f989c4caa3cdad720
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-01-15 14:43:29 +0200

    Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactPropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+class Statistics extends Component{
+    reneder() {
+        const { good, neutral, bad, total, positivePercentage } = this.props;
+        return ();
+    }
+};
+
